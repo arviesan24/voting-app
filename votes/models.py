84830e3303fd835802283be17d72e1b9c68fcc01
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 00:41:30 +0800

    Create `Vote` model

---
+++ votes/models.py
@@ -0,0 +1,16 @@
+from django.conf import settings
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
+from django.db import models
+
+
+class Vote(models.Model):
+    """Model for Votes."""
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name='votes',
+        related_query_name='vote', on_delete=models.CASCADE,
+        blank=True, null=True)
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
