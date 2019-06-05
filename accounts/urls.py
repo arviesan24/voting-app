e57f36f8ac9d9740ebce1ccd19b13372c70ec559
Romel Santoral <ctechnology24@gmail.com>
2019-06-05 19:43:23 +0800

    Register `logout` router

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,6 +6,7 @@ from . import views
 
 urlpatterns = [
     path('login/', views.LoginFormView.as_view(), name='login'),
+    path('logout/', views.LogoutFormView.as_view(), name='logout'),
     path('signup/', views.SignupFormView.as_view(), name='signup'),
     path('password/reset/',
         views.PasswordResetFormView.as_view(), name='password_reset'),
