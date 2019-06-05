cdb4601a264efd32a04b5579c34889e673c67ea4
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 00:23:14 +0800

    Create `Poll` model

---
+++ polls/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.conf import settings
+
+
+class Poll(models.Model):
+    """Model for Polls."""
+
+    owner = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE,
+        related_name='polls',
+        related_query_name='poll')
+    title = models.CharField(max_length=50, blank=False)
+    description = models.TextField(blank=False)
