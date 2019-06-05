1f8ac7d48eaf7272cd4132a86320d6e29947ecbc
Romel Santoral <ctechnology24@gmail.com>
2019-06-05 18:44:45 +0800

    Set `password_reset` router

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,7 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginFormView.as_view(), name='login'),
     path('signup/', views.SignupFormView.as_view(), name='signup'),
+    path('password/reset/',
+        views.PasswordResetFormView.as_view(), name='password_reset'),
     path('profile/', views.ProfileDetailView.as_view(), name='profile'),
 ]
