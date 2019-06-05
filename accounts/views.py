60257e63f1b5170f4aeadbafce5815b999707bbe
Romel Santoral <ctechnology24@gmail.com>
2019-06-05 15:26:25 +0800

    Remove `FormView` from imports

--- accounts/views.py
+++ accounts/views.py
@@ -4,7 +4,6 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import FormView
 
 from allauth.account.forms import LoginForm
 from allauth.account.views import LoginView
