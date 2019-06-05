04a1c8d9ba42af2de633a8368f32ac8f27924eea
Romel Santoral <ctechnology24@gmail.com>
2019-06-05 15:09:28 +0800

    Update `ProfileDetailView` route

--- accounts/urls.py
+++ accounts/urls.py
@@ -8,6 +8,5 @@ from . import views
 urlpatterns = [
     path('login/', views.LoginFormView.as_view(), name='login'),
     path('signup/', views.SignupFormView.as_view(), name='signup'),
-    path('profile/', TemplateView.as_view(
-        template_name='accounts/profile.html'), name='profile'),
+    path('profile/', views.ProfileDetailView.as_view(), name='profile'),
 ]
