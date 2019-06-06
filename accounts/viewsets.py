cd2a428e873561bc63fc04f599249a66bf10859e
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 22:49:57 +0800

    Create `IsAccountOwnerOrReadOnly`

--- accounts/viewsets.py
+++ accounts/viewsets.py
@@ -9,6 +9,21 @@ from rest_framework import permissions
 from . import serializers
 
 
+class IsAccountOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to allow only the account owner to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj == request.user
+
+
 class UserViewSet(viewsets.ModelViewSet):
     """Viewset for UserSerializer."""
 
