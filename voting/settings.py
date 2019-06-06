abd57617e6c2958b218bc011c5f6c4f49e547646
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 21:26:49 +0800

    Remove `DEFAULT_PAGINATION_CLASS` of rest framework

--- voting/settings.py
+++ voting/settings.py
@@ -155,8 +155,8 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 100
+    # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    # 'PAGE_SIZE': 100
 }
 
 
