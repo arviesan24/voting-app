c3e1e3e79db6a058e12a8c31fead4cdfcdf1ab63
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 22:21:42 +0800

    Add `polls.urls_api` in project urls

--- voting/urls.py
+++ voting/urls.py
@@ -36,6 +36,7 @@ urlpatterns = [
     path('', views.PollListView.as_view(), name='home'),
 
     # DRF URLs
+    path('', include('polls.urls_api')),
     path('api/', include(router.urls)),
     path('api-auth/',
         include('rest_framework.urls', namespace='rest_framework')),
