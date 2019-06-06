ed225e410045949dc32fdadcb8333a4d4f285d65
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 22:12:26 +0800

    Import app's models and serializers

--- polls/viewsets.py
+++ polls/viewsets.py
@@ -4,3 +4,6 @@ from django.db.models import Q
 from django_filters import rest_framework as django_filters
 from rest_framework import viewsets
 from rest_framework import permissions
+
+from . import serializers
+from . import models
