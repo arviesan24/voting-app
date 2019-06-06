f24e4c951e451f2f66e7bd31f0d2496f1895b71c
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 23:37:11 +0800

    Create `IsChoiceOwnerOrReadOnly` permission class

--- polls/viewsets.py
+++ polls/viewsets.py
@@ -24,6 +24,21 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
         return obj.owner == request.user
 
 
+class IsChoiceOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow choice owner to edit it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request,
+        # so we'll always allow GET, HEAD or OPTIONS requests.
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions are only allowed to the owner of the snippet.
+        return obj.poll.owner == request.user
+
+
 class PollViewSet(viewsets.ModelViewSet):
     """Viewset for PollSerializer"""
 
