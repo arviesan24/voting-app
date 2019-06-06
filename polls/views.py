7c577aa0e5f8318737c59ca88bdea028253105c2
Romel Santoral <ctechnology24@gmail.com>
2019-06-06 19:11:20 +0800

    Create `PollListView`

--- polls/views.py
+++ polls/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 from django.views.generic import ListView
 
 from . import models
+
+
+class PollListView(ListView):
+    """Polls ListView."""
+
+    template_name = 'home.html'
+    paginate_by = 50
+    context_object_name = 'polls'
+
+    def get_queryset(self):
+        """Return querset for the list."""
+        qs = models.Poll.objects.all().order_by('-id')
+        return qs
