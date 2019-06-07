124df4fa0bc37ba64812896bc02bb70d0b687008
Romel Santoral <ctechnology24@gmail.com>
2019-06-08 00:45:30 +0800

    Add `total_votes` field in `ChoiceSerializer`

--- polls/serializers.py
+++ polls/serializers.py
@@ -18,8 +18,15 @@ class PollSerializer(serializers.HyperlinkedModelSerializer):
 class ChoiceSerializer(serializers.HyperlinkedModelSerializer):
     """Serializer for `Choice` model."""
 
+    total_votes = serializers.SerializerMethodField()
+
     class Meta:
         model = models.Choice
         fields = (
-            'url', 'id', 'poll', 'name',
+            'url', 'id', 'poll', 'name', 'total_votes',
             'datetime_created', 'datetime_modified')
+    
+    def get_total_votes(self, obj):
+        """Return total votes per object."""
+
+        return obj.votes.count()
