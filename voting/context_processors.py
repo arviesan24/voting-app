fee7f92a817e66cfa75e65b827d84f90d46b7e5f
Romel Santoral <ctechnology24@gmail.com>
2019-06-07 23:03:25 +0800

    Create `global_custom_tags` in context_processors

--- voting/context_processors.py
+++ voting/context_processors.py
@@ -2,6 +2,12 @@
 
 from django.conf import settings
 
+
 def debug(context):
     """Return DEBUG value from settings."""
     return {'DEBUG': settings.DEBUG}
+
+
+def global_custom_tags(context):
+    """Returns additional context to templates."""
+    return {'API_ROOT_URL': settings.API_ROOT_URL}
