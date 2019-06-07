db6eefba8e60ae521d394cff457f75de999ec6ef
Romel Santoral <ctechnology24@gmail.com>
2019-06-07 23:06:34 +0800

    Add `DEBUG` to `global_custom_tags`

--- voting/context_processors.py
+++ voting/context_processors.py
@@ -10,4 +10,4 @@ def debug(context):
 
 def global_custom_tags(context):
     """Returns additional context to templates."""
-    return {'API_ROOT_URL': settings.API_ROOT_URL}
+    return {'DEBUG': settings.DEBUG, 'API_ROOT_URL': settings.API_ROOT_URL}
