568a3898108c94477c8dfe445bea2e9397ec70a4
Romel Santoral <ctechnology24@gmail.com>
2019-06-10 15:16:45 +0800

    Set variables to be used by the `choices-dropdown` component

--- static/scripts/vue.js
+++ static/scripts/vue.js
@@ -3,13 +3,20 @@ Vue.config.debug = vueDebug;
 var vm = new Vue({
     el: '.app',
     data: {
-      choices: null
+      choices: null,
+      dropdownOptions: []
     },
     mounted() {
       axios.get(`${API_ROOT_URL}choices/?poll=${pollId}`)
       .then(response => {
         this.$nextTick(() => {
           this.choices = response.data;
+          this.choices.forEach(choice => {
+            this.dropdownOptions.push({
+              value: choice.id,
+              name: choice.name
+            });
+          });
         });
       })
     }
