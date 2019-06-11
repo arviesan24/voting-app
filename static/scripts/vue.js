f63b8bb714342ac1ba7cab6ce82bd06b2194e233
Romel Santoral <ctechnology24@gmail.com>
2019-06-11 16:46:30 +0800

    Save new vote and reload chart

--- static/scripts/vue.js
+++ static/scripts/vue.js
@@ -8,23 +8,39 @@ var vm = new Vue({
       selectedChoice: Number
     },
     mounted() {
-      axios.get(`${API_ROOT_URL}choices/?poll=${pollId}`)
-      .then(response => {
-        this.$nextTick(() => {
-          this.choices = response.data;
-          this.choices.forEach(choice => {
-            this.dropdownOptions.push({
-              value: choice.id,
-              name: choice.name
-            });
-          });
-        });
-      })
+      this.loadChart();
     },
     methods: {
-      saveVote(choice) {
+      loadChart() {
+        axios.get(`${API_ROOT_URL}choices/?poll=${pollId}`)
+        .then(response => {
+          this.$nextTick(() => {
+            this.choices = response.data;
+            this.selectedChoice = this.choices[0].id;
+            this.choices.forEach(choice => {
+              this.dropdownOptions.push({
+                value: choice.id,
+                name: choice.name
+              });
+            });
+          });
+        })
+      },
+      getVote(choice) {
         // get the selected value that will be passed to the post request.
-        this.selectedChoice = choice;
+        this.selectedChoice = Number(choice);
+      },
+      saveVote() {
+        axios.defaults.xsrfCookieName = 'csrftoken';
+        axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+        axios.post(`${API_ROOT_URL}votes/`, {content_type: 'choice', object_id: this.selectedChoice})
+        .then((response) => {
+          // reinitialize the values and reload chart.
+          this.choices = null
+          this.dropdownOptions = []
+          this.selectedChoice = null
+          this.loadChart();
+        });
       }
     }
   })
