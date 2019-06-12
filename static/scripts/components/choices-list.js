9bda6d6f6715a4ed8fad76b26b2f8498e1fa4ec5
Romel Santoral <ctechnology24@gmail.com>
2019-06-12 21:33:12 +0800

    Create `choices-list` js

---
+++ static/scripts/components/choices-list.js
@@ -0,0 +1,25 @@
+Vue.component('choices-list', {
+  props: {
+    choices: Array,
+  },
+  template: `
+  <div>
+    <table class="table table-striped">
+      <tbody>
+        <tr v-for="choice in choices">
+          <th scope="row">{{choice.name}}</th>
+          <td><button class="btn btn-success btn-sm mr-2"><i class="fa fa-edit"></i></button><button v-on:click="deleteChoice(choice.id)" class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+  `,
+  methods: {
+    deleteChoice(choiceId) {
+      axios.delete(`${API_ROOT_URL}choices/${choiceId}/`, {headers: {'Authorization': auth_user_token}})
+      .then(() => {
+        this.$parent.loadChoices();
+      });
+    }
+  },
+});
