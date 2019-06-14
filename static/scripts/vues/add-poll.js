f6446609bdda4024fd080b38f3f4568a83a81d33
Romel Santoral <ctechnology24@gmail.com>
2019-06-14 19:07:49 +0800

    Create `getNewPoll` method and `newPoll` data

--- static/scripts/vues/add-poll.js
+++ static/scripts/vues/add-poll.js
@@ -3,6 +3,12 @@ Vue.config.debug = vueDebug;
 var vm = new Vue({
   el: '#app',
   data: {
-    showModal: false
-  }
+    showModal: false,
+    newPoll: {}
+  },
+  methods: {
+    getNewPoll(e) {
+      this.newPoll = e;
+    }
+  },
 })
