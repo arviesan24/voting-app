12cb4905bd63ec846f966dcbbf39311b1dbdf960
Romel Santoral <ctechnology24@gmail.com>
2019-06-14 16:24:16 +0800

    Set initial template, data and computed

---
+++ static/scripts/components/poll-add-form.js
@@ -0,0 +1,49 @@
+Vue.component('poll-add-form', {
+  template: `
+  <transition name="modal">
+    <div class="modal-mask">
+      <div class="modal-wrapper">
+        <div class="modal-container">
+
+          <div class="modal-header">
+            <slot name="header">
+              default header
+            </slot>
+          </div>
+
+          <div class="modal-body">
+            <slot name="body">
+              <input type="text" v-model="pollTitle" class="form-control my-2" placeholder="Add Title">
+              <textarea v-model="pollDescription" class="form-control my-2" placeholder="Add Description" rows="3"></textarea>
+              <textarea v-model="pollChoices" class="form-control my-2" placeholder="Add Choices (seperated by line)" rows="5"></textarea>
+            </slot>
+          </div>
+
+          <div class="modal-footer">
+            <slot name="footer">
+              <button class="btn btn-outline-info" @click="$emit('close')">
+                Cancel
+              </button>
+              <button class="btn btn-info" @click="$emit('close')">
+                Save
+              </button>
+            </slot>
+          </div>
+        </div>
+      </div>
+    </div>
+  </transition>
+  `,
+  data() {
+    return {
+      pollTitle: '',
+      pollDescription: '',
+      pollChoices: ''
+    }
+  },
+  computed: {
+    choicesArray: function() {
+      return this.pollChoices.split('\n');
+    }
+  }
+})
