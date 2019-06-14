ae4e3cdfb5ed38e628fce641a53714dd26927743
Romel Santoral <ctechnology24@gmail.com>
2019-06-14 20:02:17 +0800

    Create `SaveNewPoll`

--- static/scripts/vues/add-poll.js
+++ static/scripts/vues/add-poll.js
@@ -9,6 +9,20 @@ var vm = new Vue({
   methods: {
     getNewPoll(e) {
       this.newPoll = e;
+    },
+    saveNewPoll() {
+      axios({
+        method: "post",
+        url: `${API_ROOT_URL}polls/`,
+        headers: {
+          'Authorization': auth_user_token
+        },
+        data: {
+          owner: `${API_ROOT_URL}users/${userId}/`,
+          title: this.newPoll.title,
+          description: this.newPoll.description
+        }
+      });
     }
   },
 })
